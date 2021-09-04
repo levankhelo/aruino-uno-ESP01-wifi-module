0699d5895287227937df6ee223db2bf283895d2b
L3V14TH4N <37659795+levankhelo@users.noreply.github.com>
2021-09-04 20:42:56 +0400

    Update wifi.ino

--- wifi.ino
+++ wifi.ino
@@ -4,72 +4,75 @@ SoftwareSerial wifiSerial(2, 3);      // RX, TX for ESP8266
 
 bool DEBUG = true;   //show more logs
 int responseTime = 10; //communication timeout
+long SerialMonitorAddress = 9600; // 115200
 
 void setup()
 {
-	pinMode(13,OUTPUT);  //set build in led as output
-	// Open serial communications and wait for port to open esp8266:
-	Serial.begin(115200);
-	while (!Serial) { ; } // wait for serial port to connect. Needed for Leonardo only
-	wifiSerial.begin(115200);
+  
+  // Open serial communications and wait for port to open esp8266:
+  Serial.begin(SerialMonitorAddress);
+  while (!Serial) { ; } // wait for serial port to connect. Needed for Leonardo only
+  wifiSerial.begin(SerialMonitorAddress);
 
-	while ( !wifiSerial ) { ; } // wait for serial port to connect. Needed for Leonardo only
 
-	sendToWifi("AT+CWMODE=2", responseTime, DEBUG); // configure as access point
-	sendToWifi("AT+CIFSR", responseTime, DEBUG); // get ip address
-	sendToWifi("AT+CIPMUX=1", responseTime, DEBUG); // configure for multiple connections
-	sendToWifi("AT+CIPSERVER=1,80", responseTime, DEBUG); // turn on server on port 80
+  while ( !wifiSerial ) { ; } // wait for serial port to connect. Needed for Leonardo only
+
+  sendToWifi("AT+CWMODE=2", responseTime, DEBUG); // configure as access point
+  sendToWifi("AT+CIFSR", responseTime, DEBUG); // get ip address
+  sendToWifi("AT+CIPMUX=1", responseTime, DEBUG); // configure for multiple connections
+  sendToWifi("AT+CIPSERVER=1,80", responseTime, DEBUG); // turn on server on port 80
  
-	sendToUno("Wifi connection is running!",responseTime,DEBUG);
+  sendToUno("Wifi connection is running!",responseTime,DEBUG);
   
 
 }
 
 
 void loop() {
+  digitalWrite(13,HIGH);
 
-	if( Serial.available() > 0 ){
-		String message = readSerialMessage();
-		if( find(message, "test:")) {
+  if( Serial.available() > 0 ){
+    String message = readSerialMessage();
+    if( find(message, "test:")) {
 
-			String result = sendToWifi( message.substring(13, message.length()), responseTime, DEBUG );
-			
-			if( find(result, "OK") ) sendData("\nOK");
-			else sendData("\nEr");
-		}
-	}
+      String result = sendToWifi( message.substring(13, message.length()), responseTime, DEBUG );
+      
+      if( find(result, "OK") ) sendData("\nOK");
+      else sendData("\nEr");
+    }
+  }
 
-	if( wifiSerial.available() > 0 ) {
-	
-			String message = readWifiSerialMessage();
-		
-		if( find(message, "uno:") ) {
-			
-			String result = sendToWifi(message.substring(8,message.length()),responseTime,DEBUG);
-			
-			if(find(result,"OK")) sendData("\n"+result);
-			else sendData("\nErrRead");       				 //At command ERROR CODE for Failed Executing statement
+  if( wifiSerial.available() > 0 ) {
+  
+      String message = readWifiSerialMessage();
+    
+    if( find(message, "uno:") ) {
+      
+      String result = sendToWifi(message.substring(8,message.length()),responseTime,DEBUG);
+      
+      if(find(result,"OK")) sendData("\n"+result);
+      else sendData("\nErrRead");                //At command ERROR CODE for Failed Executing statement
 
-		} else if( find(message, "HELLO") ) {  //receives HELLO from wifi
-			
-			sendData("\\nHI!");    //arduino says HI
+    } else if( find(message, "HELLO") ) {  //receives HELLO from wifi
+      
+      sendData("\\nHI!");    //arduino says HI
 
-		} else if( find( message, "ledon" ) ) {
-			
-			//turn on built in LED:
-			digitalWrite(13,HIGH);
+    } else if( find( message, "ledon" ) ) {
+      
+      //turn on built in LED:
+      digitalWrite(13,HIGH);
 
-		} else if( find(message,"ledoff") ){
-			
-			//turn off built in LED:
-			digitalWrite(13,LOW);
+    } else if( find(message,"ledoff") ){
+      
+      //turn off built in LED:
+      digitalWrite(13,LOW);
 
-		}
-		else{
-			sendData("\nErrRead");                 //Command ERROR CODE for UNABLE TO READ
-		}
-	}
-	delay(responseTime);
+    }
+    else{
+      sendData("\nErrRead");                 //Command ERROR CODE for UNABLE TO READ
+    }
+  }
+  delay(responseTime);
 
 }
 
@@ -81,17 +84,17 @@ void loop() {
 * Returns: void
 */
 void sendData( String str ) {
-	
-	String len="";
-	len+=str.length();
-	
-	sendToWifi("AT+CIPSEND=0,"+len, responseTime, DEBUG);
-	
-	delay(100);
-	sendToWifi(str, responseTime, DEBUG);
+  
+  String len="";
+  len+=str.length();
+  
+  sendToWifi("AT+CIPSEND=0,"+len, responseTime, DEBUG);
+  
+  delay(100);
+  sendToWifi(str, responseTime, DEBUG);
 
-	delay(100);
-	sendToWifi("AT+CIPCLOSE=5", responseTime, DEBUG);
+  delay(100);
+  sendToWifi("AT+CIPCLOSE=5", responseTime, DEBUG);
 
 }
 
@@ -103,7 +106,7 @@ void sendData( String str ) {
 * Returns: true if match else false
 */
 boolean find( String string, String value ) {
-	return string.indexOf( value ) >= 0;
+  return string.indexOf( value ) >= 0;
 }
 
 
@@ -114,20 +117,20 @@ boolean find( String string, String value ) {
 * Returns: The response from the Arduino (if there is a reponse)
 */
 String readSerialMessage() {
-	
-	char value[100]; 
-	int index_count =0;
-	
-	while( Serial.available() > 0 ) {
-		value[index_count]=Serial.read();
-		index_count++;
-		value[index_count] = '\0'; // Null terminate the string
-	}
-	
-	String str(value);
-	str.trim();
-	
-	return str;
+  
+  char value[100]; 
+  int index_count =0;
+  
+  while( Serial.available() > 0 ) {
+    value[index_count]=Serial.read();
+    index_count++;
+    value[index_count] = '\0'; // Null terminate the string
+  }
+  
+  String str(value);
+  str.trim();
+  
+  return str;
 }
 
 
@@ -139,20 +142,20 @@ String readSerialMessage() {
 * Returns: The response from the esp8266 (if there is a reponse)
 */
 String readWifiSerialMessage() {
-	
-	char value[100]; 
-	int index_count = 0;
+  
+  char value[100]; 
+  int index_count = 0;
 
-	while( wifiSerial.available() > 0 ) {
-		value[index_count] = wifiSerial.read();
-		index_count++;
-		value[index_count] = '\0'; // Null terminate the string
-	}
-	
-	String str(value);
-	str.trim();
-	
-	return str;
+  while( wifiSerial.available() > 0 ) {
+    value[index_count] = wifiSerial.read();
+    index_count++;
+    value[index_count] = '\0'; // Null terminate the string
+  }
+  
+  String str(value);
+  str.trim();
+  
+  return str;
 }
 
 
@@ -164,24 +167,24 @@ String readWifiSerialMessage() {
 * Returns: The response from the esp8266 (if there is a reponse)
 */
 String sendToWifi( String command, const int timeout, boolean debug ) {
-	
-	String response = "";
-	wifiSerial.println( command ); // send the read character to the esp8266
-	long int time = millis();
+  
+  String response = "";
+  wifiSerial.println( command ); // send the read character to the esp8266
+  long int time = millis();
 
-	while( (time + timeout) > millis() ) {
-		while( wifiSerial.available() ) {
-			// The esp has data so display its output to the serial window 
-			char c = wifiSerial.read(); // read the next character.
-			response += c;
-		}  
-	}
-	
-	if( debug ) {
-		Serial.println(response);
-	}
+  while( (time + timeout) > millis() ) {
+    while( wifiSerial.available() ) {
+      // The esp has data so display its output to the serial window 
+      char c = wifiSerial.read(); // read the next character.
+      response += c;
+    }  
+  }
+  
+  if( debug ) {
+    Serial.println(response);
+  }
 
-	return response;
+  return response;
 }
 
 /*
@@ -192,24 +195,22 @@ String sendToWifi( String command, const int timeout, boolean debug ) {
 */
 String sendToUno( String command, const int timeout, boolean debug ) {
 
-	String response = "";
-	Serial.println(command); // send the read character to the esp8266
-	long int time = millis();
-	
-	while( (time + timeout) > millis() )
-	{
-		while( Serial.available() ) {
-			// The esp has data so display its output to the serial window 
-			char c = Serial.read(); // read the next character.
-			response+=c;
-		}  
-	}
+  String response = "";
+  Serial.println(command); // send the read character to the esp8266
+  long int time = millis();
+  
+  while( (time + timeout) > millis() )
+  {
+    while( Serial.available() ) {
+      // The esp has data so display its output to the serial window 
+      char c = Serial.read(); // read the next character.
+      response+=c;
+    }  
+  }
 
-	if( debug ) {
-		Serial.println(response);
-	}
-	
-	return response;
+  if( debug ) {
+    Serial.println(response);
+  }
+  
+  return response;
 }
-
-
